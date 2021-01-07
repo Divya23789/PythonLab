0ff95939c93623b1ab46e30ba20fccc52e16bef0
fullstack-lab04 <bhanu.yandra@gmail.com>
2021-01-07 01:03:55 +0000

    practice programs

---
+++ ValidateInput.py
@@ -0,0 +1,17 @@
+def user_choice():
+    choice = '11'
+    acceptable_value = range(0, 10)
+    while choice.isdigit() == False or int(choice) > 10:
+        choice = input("Input enter a number(0-10):")
+        if choice.isdigit():
+          if int(choice) in acceptable_value:
+            print("True")
+          else:
+            print("Sorry not in range")
+        else:
+            print("Sorry that is not a digit")
+
+
+    return int(choice)
+user_choice()
+

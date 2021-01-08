56982c0d7b238354dee6cba232d66830c7702c6b
fullstack-lab04 <bhanu.yandra@gmail.com>
2021-01-08 15:21:12 +0000

    practice programs

--- greater than 17 return double the absolute difference.py
+++ greater than 17 return double the absolute difference.py
@@ -1,11 +1,19 @@
 #Write a Python program to get the difference between a given number and 17,
 # if the number is greater than 17 return double the absolute difference.
 
-def seventeen():
-    print("Enter the number:")
-    x = input()
-    y = int(x)-17
-    if y > 17:
-        print(y*2)
+# def seventeen():
+#     print("Enter the number:")
+#     x = input()
+#     y = int(x)-17
+#     if y > 17:
+#         print(abs(y*2))
+# seventeen()
 
-seventeen()
+def difference(n):
+    if n <= 17:
+        return 17 - n
+    else:
+        return (n - 17) * 2
+
+print(difference(22))
+print(difference(14))
